90b84ce132249dd7ab0cf05c652705164f9acbf0
Andrew Cole <aocole@gmail.com>
2012-09-14 16:13:26 -0700

    Add CSV download and link to finish data entry

--- Gemfile
+++ Gemfile
@@ -26,6 +26,7 @@ end
 gem 'jquery-rails'
 gem 'twitter-bootstrap-rails'
 gem 'dynamic_form'
+gem 'fastercsv'
 
 # To use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
--- app/controllers/observations_controller.rb
+++ app/controllers/observations_controller.rb
@@ -1,7 +1,14 @@
+require 'fastercsv'
 class ObservationsController < ApplicationController
 
   def index
     @observations = Observation.all
+    respond_to do |format|
+      format.html
+      format.csv do
+        render_csv("upp-data-#{Time.now.strftime("%Y%m%d")}")
+      end
+    end
   end
 
   def new
@@ -48,6 +55,26 @@ class ObservationsController < ApplicationController
 
   def thanks
     
+  end
+  
+  private
+  
+  def render_csv(filename = nil)
+    filename ||= params[:action]
+    filename += '.csv'
+
+    if request.env['HTTP_USER_AGENT'] =~ /msie/i
+      headers['Pragma'] = 'public'
+      headers["Content-type"] = "text/plain" 
+      headers['Cache-Control'] = 'no-cache, must-revalidate, post-check=0, pre-check=0'
+      headers['Content-Disposition'] = "attachment; filename=\"#{filename}\"" 
+      headers['Expires'] = "0" 
+    else
+      headers["Content-Type"] ||= 'text/csv'
+      headers["Content-Disposition"] = "attachment; filename=\"#{filename}\"" 
+    end
+
+    render :layout => false
   end
 
 end
